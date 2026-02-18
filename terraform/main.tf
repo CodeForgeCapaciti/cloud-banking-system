458d6c80687bd5b6e03fc4bdb1c6a8dc838389b9
Susan04616 <susansusie04@gmail.com>
2026-02-18 11:38:41 +0200

    expose minio and mongo resource outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,11 @@ resource "docker_volume" "minio_data" {
 output "minio_volume_name" {
   value = docker_volume.minio_data.name
 }
+
+output "docker_network_name" {
+  value = docker_network.banking_network.name
+}
+
+output "mongo_volume_name" {
+  value = docker_volume.mongo_data.name
+}
