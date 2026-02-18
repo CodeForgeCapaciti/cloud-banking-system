008cf25c54090984fb7741c852e68462c1dc3033
Susan04616 <susansusie04@gmail.com>
2026-02-18 12:12:18 +0200

    Modified Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
 terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
-  }
-}
 
-provider "docker" {}
-
-resource "docker_network" "banking_network" {
-  name = "banking_network"
-}
-
-resource "docker_volume" "mongo_data" {
-  name = "mongo-data"
 }
