a9c94413be86ee2c70ef3b27c2251fae0fbd52ec
Susan04616 <susansusie04@gmail.com>
2026-02-18 11:47:51 +0200

    Update Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,26 +9,3 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_network" "banking_network" {
-  name = "banking_network"
-}
-
-resource "docker_volume" "mongo_data" {
-  name = "mongo-data"
-}
-
-resource "docker_volume" "minio_data" {
-  name = "minio-data"
-}
-
-output "minio_volume_name" {
-  value = docker_volume.minio_data.name
-}
-
-output "docker_network_name" {
-  value = docker_network.banking_network.name
-}
-
-output "mongo_volume_name" {
-  value = docker_volume.mongo_data.name
-}
